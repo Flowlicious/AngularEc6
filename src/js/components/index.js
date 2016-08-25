1e90ccf7c3e8acf2d6dcaaf20a6a13d588152a38
Florian <f.gruehn@googlemail.com>
2016-08-25 16:17:52 +0200

    heyhey

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 import ListErrors from './list-errors.component';
-
+import ShowAuthed from './show-authed.directive';
 
 let componentsModule = angular.module('app.components', []);
 componentsModule.component('listErrors',ListErrors);
+componentsModule.directive('showAuthed',ShowAuthed);
 
 export default componentsModule;
--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,7 +1,8 @@
 export default class User {
-    constructor(AppConstants, $http) {
+    constructor(AppConstants, $http,JWT,$state) {
         'ngInject';
 
+        this._JWT = JWT;
         this._AppConstants = AppConstants;
         this._$http = $http;
         this.current = null;
@@ -18,9 +19,16 @@ export default class User {
         }).then( // On success...
             (res) => {
                 // Store the user's info for easy lookup
+                this._JWT.save(res.data.user.token);
                 this.current = res.data.user;
                 return res;
             }
         );
+    }
+
+    logout(){
+      this.current = null;
+      this._JWT.destroy();
+      this._$state.go(this._$state.$current, null, { reload: true });
     }
 }
